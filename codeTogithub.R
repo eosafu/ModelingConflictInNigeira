ae8ddd0a9fe2280e76395d0dd2856c72612408aa
Osafu Egbon <70357973+eosafu@users.noreply.github.com>
2023-02-10 22:55:03 -0300

    R code

--- codeTogithub.R
+++ codeTogithub.R
@@ -40,62 +40,26 @@ spde <- inla.spde2.pcmatern(mesh = mesh,
 
 ####  Dual mesh function, sourced from R-INLA (Lindgren, F., Rue, H., and Lindstrom, J. (2011))
 ####  included here for self containment
+source("bookmesh.R")
 
-book.mesh.dual <- function(mesh) {
-  if (mesh$manifold=='R2') {
-    ce <- t(sapply(1:nrow(mesh$graph$tv), function(i)
-      colMeans(mesh$loc[mesh$graph$tv[i, ], 1:2])))
-    library(parallel)
-    pls <- mclapply(1:mesh$n, function(i) {
-      p <- unique(Reduce('rbind', lapply(1:3, function(k) {
-        j <- which(mesh$graph$tv[,k]==i)
-        if (length(j)>0) 
-          return(rbind(ce[j, , drop=FALSE],
-                       cbind(mesh$loc[mesh$graph$tv[j, k], 1] +
-                               mesh$loc[mesh$graph$tv[j, c(2:3,1)[k]], 1], 
-                             mesh$loc[mesh$graph$tv[j, k], 2] +
-                               mesh$loc[mesh$graph$tv[j, c(2:3,1)[k]], 2])/2))
-        else return(ce[j, , drop=FALSE])
-      })))
-      j1 <- which(mesh$segm$bnd$idx[,1]==i)
-      j2 <- which(mesh$segm$bnd$idx[,2]==i)
-      if ((length(j1)>0) | (length(j2)>0)) {
-        p <- unique(rbind(mesh$loc[i, 1:2], p,
-                          mesh$loc[mesh$segm$bnd$idx[j1, 1], 1:2]/2 +
-                            mesh$loc[mesh$segm$bnd$idx[j1, 2], 1:2]/2, 
-                          mesh$loc[mesh$segm$bnd$idx[j2, 1], 1:2]/2 +
-                            mesh$loc[mesh$segm$bnd$idx[j2, 2], 1:2]/2))
-        yy <- p[,2]-mean(p[,2])/2-mesh$loc[i, 2]/2
-        xx <- p[,1]-mean(p[,1])/2-mesh$loc[i, 1]/2
-      }
-      else {
-        yy <- p[,2]-mesh$loc[i, 2]
-        xx <- p[,1]-mesh$loc[i, 1]
-      }
-      Polygon(p[order(atan2(yy,xx)), ])
-    })
-    return(SpatialPolygons(lapply(1:mesh$n, function(i)
-      Polygons(list(pls[[i]]), i))))
-  }
-  else stop("It only works for R2!")
-}
-
-
+# Obtain dual mesh
 dmesh        <- book.mesh.dual(mesh=mesh)
 window.polys <- Polygons(list(Polygon(loc.2D)), '0')
 windowSP     <- SpatialPolygons(list(window.polys))
-
+# Determine weight
 weight <- sapply(1:length(dmesh), function(i) {
   if (gIntersects(dmesh[i, ], windowSP))
     return(gArea(gIntersection(dmesh[i, ], windowSP)))
   else return(0)
 })
-
+# Augumented response
 response <- rep(0:1, c(nfield, n))
 offset.response <- c(weight, rep(0, n)) 
 unobserved.A <- Diagonal(nfield, rep(1, nfield))
 observed.A <- inla.spde.make.A(mesh, xy)
+# Stack projection matrices
 A <- rbind(unobserved.A, observed.A)
+# Data stack for INLA
 datastack <- inla.stack(
   data = list(y = response, e = offset.response), 
   A = list(1, A),
